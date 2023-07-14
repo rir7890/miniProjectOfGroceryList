ff4c2a8730b5b569c14e3974bebbee4ca18ddd2b
Rahul Kumar <rir7890@gmail.com>
2023-07-14 19:59:57 +0530

    grocery list done

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  //   const headerStyle = {
+  //     backgroundColor: "royalblue",
+  //     color: "#fff",
+  //   };
+  return (
+    // <header
+    //   //   style={{
+    //   //     backgroundColor: "mediumblue",
+    //   //     color: "#fff",
+    //   //   }}
+    //   style={headerStyle}
+    // >
+    <header>
+      <h1>Groceries List</h1>
+    </header>
+  );
+};
+
+export default Header;
